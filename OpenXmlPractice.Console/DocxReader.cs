44c272263f7d190fe17180bb12ddc4a059b91e4b
vokabud <etecture.dubakov@extaccount.com>
2024-07-20 15:09:48 +0300

    Initial DocX read

---
+++ OpenXmlPractice.Console/DocxReader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace OpenXmlPractice.Console;
+
+internal static class DocxReader
+{
+    public static void Read(string filePath,  string fileName)
+    {
+        using (var wordDoc = WordprocessingDocument.Open($@"{filePath}\{fileName}", false))
+        {
+            // Get the main document part
+            var mainPart = wordDoc.MainDocumentPart;
+
+            if (mainPart == null)
+            {
+                return;
+            }
+
+            var controls = mainPart
+                .Document
+                .Body
+                .Descendants<SdtElement>();
+
+            foreach (var control in controls)
+            {
+                string controlName = control.SdtProperties.GetFirstChild<SdtAlias>()?.Val?.Value;
+                string controlValue = control.Descendants<Text>().Select(t => t.Text).FirstOrDefault();
+
+                System.Console.WriteLine($"Control Name: {controlName}");
+                System.Console.WriteLine($"Control Value: {controlValue}");
+                System.Console.WriteLine("------------------------------");
+            }
+        }
+    }
+}
---
+++ OpenXmlPractice.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OpenXmlPractice.Console;
+
+Console.WriteLine("Hello, World!");
+
+
+var filePath = @"G:\Solutions\my-openxml-practice\Examples";
+var fileName = "Report.docx";
+
+DocxReader.Read(filePath, fileName);
+
